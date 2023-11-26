21c8d9b1f1c85637be79b4cb35e7a75c5bbaf94b
Ahmet Can ALSAT <ahmetcan_045@hotmail.com>
2023-11-26 20:00:21 +0300

    Update Form1.cs
    KATEGORİLER İSİMLİ FORMU ÇAĞIRABİLMEK İÇİN CATEGORİES İSİMLİ BUTON TIKLANDIĞINDA İÇİNDE GERÇEKLEŞECEK KOMUTLAR YAZILDI

--- PROJE_SQL_DB/PROJE_SQL_DB/Form1.cs
+++ PROJE_SQL_DB/PROJE_SQL_DB/Form1.cs
@@ -16,5 +16,17 @@ namespace PROJE_SQL_DB
         {
             InitializeComponent();
         }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btn_categories_Click(object sender, EventArgs e)
+        {
+            frmCategories fr = new frmCategories(); // frmCategories isimli formumuzu çağırabilmek için form1 içinde fr ismiyle tanımladık.
+            fr.Show(); // fr isimli değişkeni göstertmek için kullanılır.
+            
+        }
     }
 }
