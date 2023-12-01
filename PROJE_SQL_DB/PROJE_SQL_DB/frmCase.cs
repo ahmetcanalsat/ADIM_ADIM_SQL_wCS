3f2831498cb30792b6ac571f62aee81434c1fc37
Ahmet Can ALSAT <ahmetcan_045@hotmail.com>
2023-12-01 11:33:55 +0300

    Create frmCase.cs
    günlük ciroyu görmek için kasa tablosundan veri çekip form ekranına yazdırdık.

---
+++ PROJE_SQL_DB/PROJE_SQL_DB/frmCase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PROJE_SQL_DB
+{
+    public partial class frmCase : Form
+    {
+        public frmCase()
+        {
+            InitializeComponent();
+        }
+        SqlConnection baglanti = new SqlConnection(@"Data Source=DESKTOP-9V0QTA2\MSSQL2022;Initial Catalog=SatisVT;Integrated Security=True");
+        private void frmCase_Load(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            SqlCommand ciro_goster= new SqlCommand("SELECT * FROM TBLKASA",baglanti);
+            SqlDataReader dr = ciro_goster.ExecuteReader();
+            while (dr.Read())
+            {
+                label2.Text = (dr["TOPLAM"].ToString());
+            }
+            baglanti.Close();
+        }
+    }
+}
